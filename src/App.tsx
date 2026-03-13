949db9209d5e9ffe5c5f8a754fdee8a736119c24
JuliaTBarros <162466823+JuliaTBarros@users.noreply.github.com>
2026-03-13 15:39:58 -0300

    add index.css for global styles and update layout in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ export default function App() {
     return (
         <div className="bg-slate-900 min-h-screen w-screen text-white flex p-8 gap-8">
             {/* Lado Esquerdo: Pomodoro Timer (Ocupará 2/3) */}
-            <main className="flex-2 bg-slate-800 rounded-xl border border-slate-700 p-6 shadow-lg">
+            <main className="flex-[2] bg-slate-800 rounded-xl border border-slate-700 p-6 shadow-lg">
                 <h1 className="text-2xl font-bold mb-4">Timer em breve...</h1>
             </main>
 
             {/* Lado Direito: Todo List (Ocupará 1/3) */}
-            <aside className="flex-1 bg-slate-800 rounded-xl border border-slate-700 p-6 shadow-lg flex flex-col">
+            <aside className="flex-[1] bg-slate-800 rounded-xl border border-slate-700 p-6 shadow-lg flex flex-col">
                 <h2 className="text-xl font-bold mb-4">Fila de Tarefas</h2>
                 <div className="flex-1 overflow-y-auto">
                     {/* A lista de tarefas entrará aqui */}
--- src/frontend.tsx
+++ src/frontend.tsx
@@ -1,3 +1,4 @@
+import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Pomodoro from "./App"; // Importando o componente padrão do App.tsx
